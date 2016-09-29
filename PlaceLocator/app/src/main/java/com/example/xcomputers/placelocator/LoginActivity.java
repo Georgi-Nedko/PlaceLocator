47abc70f6e80d56d89e58a769b16f2363cfe89de
Georgi Nikolov <project_it@abv.bg>
2016-09-29 13:13:26 +0300

    working google login version

--- PlaceLocator/app/src/main/java/com/example/xcomputers/placelocator/LoginActivity.java
+++ PlaceLocator/app/src/main/java/com/example/xcomputers/placelocator/LoginActivity.java
@@ -3,21 +3,25 @@ package com.example.xcomputers.placelocator;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Typeface;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
+
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.location.LocationServices;
 
 
@@ -30,7 +34,6 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
     SignInButton signInButton;
     private boolean loggedIn = false;
     private String accName;
-    TextView tekstche;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +43,6 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
         welcomeTV = (TextView) findViewById(R.id.welcomeTV);
         Typeface custom_font = Typeface.createFromAsset(getAssets(), "fonts/Pacifico.ttf");
         welcomeTV.setTypeface(custom_font);
-        tekstche = (TextView) findViewById(R.id.tekstche);
 
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -86,7 +88,6 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
         if (requestCode == RC_SIGN_IN) {
             GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
             handleSignInResult(result);
-            Log.e("tag",result.getStatus().getStatusCode() + "");
             changeScreen(true);
         }
 
@@ -98,8 +99,7 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
         if (result.isSuccess()) {
             // Signed in successfully, show authenticated UI.
             GoogleSignInAccount acct = result.getSignInAccount();
-            accName = acct.getDisplayName();
-            tekstche.setText(acct.getDisplayName());
+           accName = acct.getDisplayName();
             Log.e("TAG", accName);
             //updateUI(true);
         } else {
