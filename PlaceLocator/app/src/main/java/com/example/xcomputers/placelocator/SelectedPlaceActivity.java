e050642cae7370ab329e3f886ca597b8344f9780
DESKTOP-JIEHJLH\svetlio <brymchevnedko@gmail.com>
2016-10-09 08:54:44 +0300

    Change the ScrollView with ViewFlipper!

--- PlaceLocator/app/src/main/java/com/example/xcomputers/placelocator/SelectedPlaceActivity.java
+++ PlaceLocator/app/src/main/java/com/example/xcomputers/placelocator/SelectedPlaceActivity.java
@@ -1,26 +1,26 @@
+
 package com.example.xcomputers.placelocator;
 
-import android.animation.Animator;
-import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Point;
 import android.location.Location;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.Display;
 import android.view.View;
-import android.view.ViewTreeObserver;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ViewFlipper;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -38,7 +38,6 @@ import java.util.Scanner;
 import io.techery.properratingbar.ProperRatingBar;
 
 public class SelectedPlaceActivity extends AppCompatActivity {
-
     private HorizontalScrollView hsv;
     private Button call;
     private Button website;
@@ -57,6 +56,7 @@ public class SelectedPlaceActivity extends AppCompatActivity {
     private View categoriesRecyclerViewAdapter;
     //private String placeSelectedID;
     private static Float kilometers;
+    private LinearLayout imagesLL;
 
     String uri;
     ImageView mImageView;
@@ -64,6 +64,9 @@ public class SelectedPlaceActivity extends AppCompatActivity {
     HorizontalScrollView imagesHSV;
     ArrayList<Integer> images;
     int counterImages = 0;
+    int counter=1;
+    int counterEven = 3;
+    int counterOdd = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -75,28 +78,84 @@ public class SelectedPlaceActivity extends AppCompatActivity {
 
         //TODO change the placeid with placeSelectedID
 
+        final ViewFlipper MyViewFlipper = (ViewFlipper)findViewById(R.id.details_photo_scroll_view);
+
+       // final Button buttonAutoFlip = (Button)findViewById(R.id.buttonautoflip);
+        //Button button1 = (Button)findViewById(R.id.button1);
+        //Button button2 = (Button)findViewById(R.id.button2);
+        final ArrayList<Integer> images;
+        Animation animationFlipIn  = AnimationUtils.loadAnimation(this, R.anim.flipin);
+        Animation animationFlipOut = AnimationUtils.loadAnimation(this, R.anim.flipout);
+        MyViewFlipper.setInAnimation(animationFlipIn);
+        MyViewFlipper.setOutAnimation(animationFlipOut);
+        MyViewFlipper.setFlipInterval(1500);
+
+        images = new ArrayList<>();
+        images.add(R.drawable.airport);
+        images.add(R.drawable.aquarium);
+        images.add(R.drawable.art_gallery);
+        images.add(R.drawable.amusement);
+
+        for(int i =0;i < images.size();i++){
+            ImageView newImageView = new ImageView(SelectedPlaceActivity.this);
+            newImageView.setBackgroundResource(images.get(i));
+            MyViewFlipper.addView(newImageView);
+        }
+        MyViewFlipper.startFlipping();
+
+
+
+
+
+
+
+//        button1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View arg0) {
+//                // TODO Auto-generated method stub
+//                MyViewFlipper.showNext();
+//            }});
+//
+//
+//        button2.setOnClickListener(new Button.OnClickListener(){
+//
+//            @Override
+//            public void onClick(View arg0) {
+//                // TODO Auto-generated method stub
+//                MyViewFlipper.showNext();
+//
+//               // MyViewFlipper.showPrevious();
+//            }});
+//        button3.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                MyViewFlipper.showNext();
+//            }
+//        });
+
+
 
 
 
 
         //Toast.makeText(SelectedPlaceActivity.this, placeInfo, Toast.LENGTH_SHORT).show();
-        String placeSelectedID = getIntent().getStringExtra("json");
-        Log.e("JSON", placeSelectedID);
+        String placeSelectedJSON = getIntent().getStringExtra("json");
+        Log.e("JSON", placeSelectedJSON);
         lastLocation = getIntent().getParcelableExtra("lastLocation");
         Log.e("LAST LOCATION", String.valueOf(lastLocation.getLatitude())+ "," +String.valueOf(lastLocation.getLongitude()));
         placeLocation = getIntent().getParcelableExtra("placeLocation");
         Log.e("PlACE LOCATION", String.valueOf(placeLocation.getLatitude()+ ","+String.valueOf(placeLocation.getLongitude())));
-       // kilometers = (lastLocation.distanceTo(placeLocation))/1000;
-       // Toast.makeText(SelectedPlaceActivity.this, placeSelectedID, Toast.LENGTH_SHORT).show();
+        // kilometers = (lastLocation.distanceTo(placeLocation))/1000;
+        // Toast.makeText(SelectedPlaceActivity.this, placeSelectedID, Toast.LENGTH_SHORT).show();
 
         //TODO REQUUEST_DENIED
         //new RequestTask().execute("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=40.6655101,-73.89188969999998&destinations=40.6905615%2C-73.9976592%7C40.6905615%2C-73.9976592%7C40.6905615%2C-73.9976592%7C40.6905615%2C-73.9976592%7C40.6905615%2C-73.9976592%7C40.6905615%2C-73.9976592%7C40.659569%2C-73.933783%7C40.729029%2C-73.851524%7C40.6860072%2C-73.6334271%7C40.598566%2C-73.7527626%7C40.659569%2C-73.933783%7C40.729029%2C-73.851524%7C40.6860072%2C-73.6334271%7C40.598566%2C-73.7527626&key=AIzaSyDWeC1Uu7iVM2HyHi-dc6Xvde6b45vSFl4");
-        new RequestTask().execute("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins="+ String.valueOf(lastLocation.getLatitude())+ ","+String.valueOf(lastLocation.getLongitude() + "&destinations="+ String.valueOf(placeLocation.getLatitude())+ "," +String.valueOf(placeLocation.getLongitude() +"&key=AIzaSyDWeC1Uu7iVM2HyHi-dc6Xvde6b45vSFl4")));
+        //new RequestTask().execute("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins="+ String.valueOf(lastLocation.getLatitude())+ ","+String.valueOf(lastLocation.getLongitude() + "&destinations="+ String.valueOf(placeLocation.getLatitude())+ "," +String.valueOf(placeLocation.getLongitude() +"&key=AIzaSyDWeC1Uu7iVM2HyHi-dc6Xvde6b45vSFl4")));
         // Log.e("mytask", mytask.getStatus().toString());
 
 
 
-        hsv = (HorizontalScrollView) findViewById(R.id.details_photo_scroll_view);
+      //  hsv = (HorizontalScrollView) findViewById(R.id.details_photo_scroll_view);
         call = (Button) findViewById(R.id.details_dial_button);
         website = (Button) findViewById(R.id.details_website_url);
         myRB = (RatingBar) findViewById(R.id.details_rating_rb);
@@ -107,15 +166,15 @@ public class SelectedPlaceActivity extends AppCompatActivity {
         nameScrollTextView = (TextView) findViewById(R.id.nameTextView);
         openTimeTV = (TextView) findViewById(R.id.details_open);
         distanceTV = (TextView) findViewById(R.id.details_ETA);
-
-      //  distanceTV.setText(""+ kilometers + "KM");
+       // imagesLL = (LinearLayout) findViewById(R.id.details_photo_holder);
+        //  distanceTV.setText(""+ kilometers + "KM");
 
 
         // phoneTV = (TextView) findViewById(R.id.det);
         //cashRB = (ProperRatingBar) findViewById(R.id.MyCashRatingBar);
         // categoriesRecyclerViewAdapter = findViewById(R.id.recycler_view_selected_place);
-        //mImageView = (ImageView) findViewById(R.id.imageViewInSV1);
-        //  mImageView2 = (ImageView) findViewById(R.id.imageViewInSV2);
+      //  mImageView = (ImageView) findViewById(R.id.image1);
+        //  mImageView2 = (ImageView) findViewById(R.id.image2);
         // imagesHSV = (LinearLayout) findViewById(R.id.imagesLinearLayout);
 //        uri = (TextView) findViewById(R.id.uriTV);
 //        images = new ArrayList<>();
@@ -135,13 +194,9 @@ public class SelectedPlaceActivity extends AppCompatActivity {
 
 
         try {
-            JSONObject jObject = new JSONObject(placeSelectedID);
+            JSONObject jObject = new JSONObject(placeSelectedJSON);
             JSONObject jObjectResult = (JSONObject) jObject.get("result");
 
-            //JSONArray reviews = (jObjectResult.getJSONArray("reviews"));
-            // if(reviews.length() == 0 || reviews.isNull(0)){
-            //    Toast.makeText(SelectedPlaceActivity.this, "reviews are null", Toast.LENGTH_SHORT).show();
-            //    reviewsTV.setText("There are no reviews!");
             if (jObjectResult.isNull("formatted_phone_number")) {
                 phoneTV = "no phone";
                 if (jObjectResult.isNull("formatted_address")) {
@@ -285,8 +340,8 @@ public class SelectedPlaceActivity extends AppCompatActivity {
 //                    }
 //                });
             // Log.e("URI",uri);
-           // cashRB.setRating((int) jObjectResult.getDouble("price_level"));
-          //  Log.e("PRICE_LEVEL",jObjectResult.getInt("price_level") + "");
+            // cashRB.setRating((int) jObjectResult.getDouble("price_level"));
+            //  Log.e("PRICE_LEVEL",jObjectResult.getInt("price_level") + "");
 
 
 
@@ -297,6 +352,8 @@ public class SelectedPlaceActivity extends AppCompatActivity {
             //cashRB.setRating(4);
             e1.printStackTrace();
         }
+        distanceTV.setText(getIntent().getStringExtra("distance") +  " , "+ getIntent().getStringExtra("duration"));
+
 
 
 
@@ -338,71 +395,125 @@ public class SelectedPlaceActivity extends AppCompatActivity {
             }
         });
 
-        hsv.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
-
-            @Override
-            public void onGlobalLayout() {
-
-
-
-
-                hsv.getViewTreeObserver().removeOnGlobalLayoutListener(this);
-                hsv.getHeight(); //height is ready
-                // Toast.makeText(SelectedPlaceActivity.this, hsv.getHeight() + "", Toast.LENGTH_SHORT).show();
-                Display display = getWindowManager().getDefaultDisplay();
-                Point size = new Point();
-                display.getSize(size);
-                int width = size.x;
-                int height = size.y;
-
-                //  Toast.makeText(SelectedPlaceActivity.this, hsv.getChildAt(0).getHeight() + "", Toast.LENGTH_SHORT).show();
-            //    mImageView.setLayoutParams(new LinearLayout.LayoutParams(width,hsv.getHeight()));
-              //  mImageView2.setLayoutParams(new LinearLayout.LayoutParams(width,hsv.getHeight()));
-
-                final ObjectAnimator animator=ObjectAnimator.ofInt(hsv, "scrollX",width);
-                animator.setDuration(15000);
-                animator.setRepeatCount(ObjectAnimator.INFINITE);
-
-
-               animator.addListener(new Animator.AnimatorListener() {
-                    @Override
-                    public void onAnimationStart(Animator animation) {
-
-                    }
-
-                    @Override
-                    public void onAnimationEnd(Animator animation) {
+//        hsv.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
 //
-                        Log.e("NEDKO","GENIQ");
-
-                    }
-
-                    @Override
-                    public void onAnimationCancel(Animator animation) {
-
-                    }
-
-                    @Override
-                    public void onAnimationRepeat(Animator animation) {
-
-                       // mImageView.setBackgroundResource(images.get(counterImages));
-                       // counterImages++;
-                       // mImageView2.setBackgroundResource(images.get(counterImages));
-                       // counterImages++;
-
-                        Log.e("NEDKO","GENIdsasdadasdas");
-                      //  if(counterImages >= images.size()-1){
-                           // counterImages=0;
-                       // }
-                        hsv.scrollTo(0,0);
-                    }
-                });
-                animator.start();
-                //animator.setRepeatCount(ObjectAnimator.INFINITE);
-                //animator.start();
-
-            }
-        });
+//            @Override
+//            public void onGlobalLayout() {
+//
+//                hsv.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+//                hsv.getHeight(); //height is ready
+//                // Toast.makeText(SelectedPlaceActivity.this, hsv.getHeight() + "", Toast.LENGTH_SHORT).show();
+//                Display display = getWindowManager().getDefaultDisplay();
+//                Point size = new Point();
+//                display.getSize(size);
+//                final int width = size.x;
+//                int height = size.y;
+//
+//                //  Toast.makeText(SelectedPlaceActivity.this, hsv.getChildAt(0).getHeight() + "", Toast.LENGTH_SHORT).show();
+//                mImageView.setLayoutParams(new LinearLayout.LayoutParams(width,hsv.getHeight()));
+//                mImageView2.setLayoutParams(new LinearLayout.LayoutParams(width,hsv.getHeight()));
+//
+//                final ObjectAnimator animator=ObjectAnimator.ofInt(hsv, "scrollX",width);
+//                animator.setDuration(6000);
+//                //animator.setRepeatCount(ObjectAnimator.INFINITE);
+//
+//                Log.e("WIDTH",width + "");
+//
+//
+//                animator.addListener(new Animator.AnimatorListener() {
+//                     int maxWidth = counter*width;
+//                    @Override
+//                    public void onAnimationStart(Animator animation) {
+////                        hsv.scrollTo(maxWidth,0);
+////                        counter+=1;
+////
+////                        Log.e("NEDKO","GENIdsasdadasdas");
+////                        if(maxWidth == width*counterEven){
+////                            counterEven+=2;
+////                            ImageView newImg = new ImageView(SelectedPlaceActivity.this);
+////                            newImg.setBackgroundResource(images.get(counterImages));
+////                            imagesLL.addView(newImg,maxWidth,hsv.getHeight());
+////
+////
+////                            counterImages++;
+////                        }
+////                        if(maxWidth == width*counterOdd){
+////                            counterOdd+=2;
+////                            ImageView newImg = new ImageView(SelectedPlaceActivity.this);
+////                            newImg.setBackgroundResource(images.get(counterImages));
+////                            imagesLL.addView(newImg,maxWidth,hsv.getHeight());
+//////                            mImageView.setBackgroundResource(images.get(counterImages));
+////                            counterImages++;
+////                        }
+////                        maxWidth = counter*width;
+////                        if(counterImages >= images.size()-1){
+////                            counterImages=0;
+////                        }
+////                        Log.e("COUNTER", counter + "");
+////                        Log.e("COUNTEREVEN", counterEven + "");
+////                        Log.e("COUNTERODD", counterOdd + "");
+////                        Log.e("COUNTERMAXWIDTH", maxWidth + "");
+////                        Log.e("NEDKO","GENIQ");
+////
+////                       // animator.start();
+//                    }
+//
+//                    @Override
+//                    public void onAnimationEnd(Animator animation) {
+////
+////                         hsv.scrollTo(maxWidth,0);
+////                        counter+=2;
+////
+////                        Log.e("NEDKO","GENIdsasdadasdas");
+////                        if(maxWidth == width*counterEven){
+////                            counterEven+=2;
+////                            ImageView newImg = new ImageView(SelectedPlaceActivity.this);
+////                            newImg.setBackgroundResource(images.get(counterImages));
+////                            imagesLL.addView(newImg,maxWidth,hsv.getHeight());
+////
+////
+////                            counterImages++;
+////                        }
+////                        if(maxWidth == width*counterOdd){
+////                            counterOdd+=2;
+////                            ImageView newImg = new ImageView(SelectedPlaceActivity.this);
+////                            newImg.setBackgroundResource(images.get(counterImages));
+////                            imagesLL.addView(newImg,maxWidth,hsv.getHeight());
+//////                            mImageView.setBackgroundResource(images.get(counterImages));
+//////                            counterImages++;
+////                        }
+////                        maxWidth = counter*width;
+////                        if(counterImages >= images.size()-1){
+////                            counterImages=0;
+////                        }
+////                        Log.e("COUNTER", counter + "");
+////                        Log.e("COUNTEREVEN", counterEven + "");
+////                        Log.e("COUNTERODD", counterOdd + "");
+////                        Log.e("COUNTERMAXWIDTH", maxWidth + "");
+////                        Log.e("NEDKO","GENIQ");
+//
+//                    }
+//
+//                    @Override
+//                    public void onAnimationCancel(Animator animation) {
+//
+//                    }
+//
+//                    @Override
+//                    public void onAnimationRepeat(Animator animation) {
+//
+////                         mImageView.setBackgroundResource(images.get(counterImages));
+////                         counterImages++;
+//
+//
+//                    }
+//                });
+//                animator.start();
+//                //animator.setRepeatCount(ObjectAnimator.INFINITE);
+//                //animator.start();
+//
+//            }
+//        });
 
 //        hsv.postDelayed(new Runnable() {
 //            @Override
@@ -488,8 +599,7 @@ public class SelectedPlaceActivity extends AppCompatActivity {
                 double distanceInKM = ((Double.parseDouble(distanceInMiles))*1.609344);
                 double distanceInKMToSecondSymbol = Math.floor(distanceInKM * 100) / 100;
                 Log.e("distanceInKM",distanceInKM+"");
-                distanceTV.setText(""+ distanceInKMToSecondSymbol + "KM,  "+ elementsArray.getJSONObject(0).getJSONObject("duration").getString("text"));
-               // distanceTV.setText(obj.getJSONArray("rows").getJSONArray(0).getJSONObject(0).getString("distance"));
+                // distanceTV.setText(obj.getJSONArray("rows").getJSONArray(0).getJSONObject(0).getString("distance"));
             } catch (JSONException e) {
                 e.printStackTrace();
             }
@@ -538,7 +648,4 @@ public class SelectedPlaceActivity extends AppCompatActivity {
             return getImageBitmap(strings[0]);
         }
     }
-}///////////////////////////
-
-
-
+}
